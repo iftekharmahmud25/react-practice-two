ab348dbdfb5bcf3c3e8f381bab6ddde8101a547a
iftekhar mahmud <iftekharmahmud98@gmail.com>
2023-09-09 16:35:49 +0600

    added file

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+ 
+
+  return (
+    <>
+    <h1 className="text-center text-4xl">hello</h1>
+    <button className="btn btn-accent">Accent</button>
+   
+    </>
+  )
+}
+
+export default App
